04e81ff06c6c062ecfd3f37c8de851fe0738174b
jiccasa <jiccasa@gmail.com>
2016-11-12 16:07:24 -0800

    ciiiiiii

---
+++ basic/circle.py
@@ -0,0 +1,9 @@
+#Circle
+
+radius = float(input('What is the radius of the circle'))
+diameter = radius * 2
+circumference = 22/7 * diameter
+area = 22/7 * radius**2
+print(diameter)
+print(circumference)
+print(area)
